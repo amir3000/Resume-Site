49c36ecb7e9daddb6bd3e0a26e88b3baffd0001a
amir3000 <amir2qi@gmail.com>
2022-07-14 19:59:20 +0430

    contact 50%

--- my-resume-site/src/components/pages/Contact/index.jsx
+++ my-resume-site/src/components/pages/Contact/index.jsx
@@ -1,12 +1,90 @@
 import React from "react";
+import {
+  SectionContainer,
+  FormWrapper,
+  FormElements,
+  H3titleWrapp,
+  H3Title,
+  FormFileds,
+  FiledWrapper,
+  FiledMargin,
+  InputFiled,
+  BtnSubmit,
+  MessageInput,
+} from "./ContactElements";
 //  import ContactElements from "ContactElements";
 const Contact = () => {
   return (
     <>
-      <form>
-        <label>Name</label>
-        <input type="text" placeholder="Enter your name" />
-      </form>
+      <SectionContainer>
+        <FormWrapper id="frm-wrp">
+          <FormElements>
+            <H3titleWrapp id="h3-wrp">
+              <H3Title>Get in Touch</H3Title>
+            </H3titleWrapp>
+
+            <FormFileds id="frm-fileds">
+              <FiledWrapper>
+                <FiledMargin>
+                  <InputFiled
+                    type="text"
+                    name="name"
+                    placeholder="Your Name"
+                    required
+                  />
+                </FiledMargin>
+              </FiledWrapper>
+            </FormFileds>
+
+            <FormFileds>
+              <FiledWrapper>
+                <FiledMargin>
+                  <InputFiled
+                    type="Email"
+                    name="Email"
+                    placeholder="Eamil:"
+                    required
+                  />
+                </FiledMargin>
+              </FiledWrapper>
+            </FormFileds>
+
+            <FormFileds>
+              <FiledWrapper>
+                <FiledMargin>
+                  <InputFiled
+                    type="text"
+                    name="Subject"
+                    placeholder="Subject:"
+                    required
+                  />
+                </FiledMargin>
+              </FiledWrapper>
+            </FormFileds>
+
+            <FormFileds>
+              <FiledWrapper>
+                <FiledMargin>
+                  <MessageInput
+                    rows="6"
+                    name="message"
+                    placeholder=" Your message"
+                    required
+                  />
+                </FiledMargin>
+              </FiledWrapper>
+            </FormFileds>
+
+            <FormFileds>
+              <FiledWrapper>
+                <FiledMargin>
+                  <BtnSubmit type="submit" value="Send Message" />
+                </FiledMargin>
+              </FiledWrapper>
+            </FormFileds>
+          </FormElements>
+        </FormWrapper>
+      </SectionContainer>
     </>
   );
 };
